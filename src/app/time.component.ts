60effec6265b7c4f18dfdc639465f4e5b18f0cc8
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2023-06-14 07:55:27 +0200

    [destroy_ref] after implementation

--- src/app/time.component.ts
+++ src/app/time.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 import { DatePipe } from '@angular/common';
-import { interval, Subject, takeUntil } from 'rxjs';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'cur-time',
@@ -8,22 +10,18 @@ import { interval, Subject, takeUntil } from 'rxjs';
   imports: [DatePipe],
   template: `{{ now | date : 'longTime' }}`,
 })
-export class TimeComponent implements OnDestroy {
-  private destroy$ = new Subject<void>();
-
+export class TimeComponent implements OnInit {
   now = new Date();
+  destroyRef = inject(DestroyRef);
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit(): void {
     interval(1000)
-      .pipe(takeUntil(this.destroy$))
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(() => {
         this.now = new Date();
         console.log('Time Stamp :', this.now.getTime());
       });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
