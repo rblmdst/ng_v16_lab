6e9639a495b85fb8b89b6e8d294af2041e57b185
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2023-11-23 20:29:45 +0100

    Final Implementation

--- src/app/Product/product-list/product-list.component.ts
+++ src/app/Product/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { ProductItemComponent } from '../product-item/product-item.component';
 import { CodeLabel, Product, ProductType } from '../product.model';
 
@@ -21,35 +21,52 @@ export class ProductListComponent {
     { id: 7, name: 'Orangina 50 Cl', type: 'drink' },
   ];
 
-  products = this.defaultValue;
+  products = signal(this.defaultValue);
 
-  selectedProductType: ProductType | 'all' = 'all';
+  selectedProductType = signal<ProductType | 'all'>('all');
 
   // TODO: Create a filtered list based on products and selectedProductType
-  filteredList = [];
+  filteredList = computed(() => {
+    const products = this.products();
+    const selectedType = this.selectedProductType();
+    return selectedType !== 'all'
+      ? products.filter((product) => product.type === selectedType)
+      : products;
+  });
 
   constructor() {
     // TOD0: Log product list changes (effect)
+    effect(() => {
+      console.log('Chganged', this.filteredList());
+    });
   }
   removeAllProducts() {
-    // TODO: remove all products from the list
+    this.products.set([]);
   }
 
   resetProductList() {
-    // TODO: set product list to default
+    this.products.set(this.defaultValue);
   }
 
   // people that are fan of using immutability :)
   addProduct() {
-    const newProduct = { id: 28, name: 'Coca Cola 50 Cl', type: 'drink' };
-    // TODO: Update the list by adding newProduct (use the existing list)
+    const newProduct: Product = {
+      id: 28,
+      name: 'Coca Cola 50 Cl',
+      type: 'drink',
+    };
+    this.products.update((oldList) => [...oldList, newProduct]);
   }
 
   onDelete(productId: number) {
     // TODO: Update the list of produts
+    this.products.update((oldList) =>
+      oldList.filter((product) => product.id !== productId)
+    );
   }
 
   onFilterValueChange(productType: string) {
     // TODO: update the filter (selectedProductType)
+    this.selectedProductType.set(productType as ProductType | 'all');
   }
 }
